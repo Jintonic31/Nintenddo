12f2b5e76827e0d849a26b4a86acb2e53071b563
JAVA <JAVA@DESKTOP-VHNHFON>
2024-03-08 14:52:20 +0900

    [장유진]Heading, Footing, Index CSS 폴더, 파일 추가

--- Nintenddo/client/src/Component/Footing.js
+++ Nintenddo/client/src/Component/Footing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../Style/'
+import '../Style/includes/footing'
 
 function Footing() {
     return (
--- Nintenddo/client/src/Component/Heading.js
+++ Nintenddo/client/src/Component/Heading.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
+import '../Style/includes/heading'
+
 
 
 function Heading() {
