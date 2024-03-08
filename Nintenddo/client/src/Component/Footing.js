76338108e3921d49a21f7e5470f53fb326deb1b6
JAVA <JAVA@DESKTOP-VHNHFON>
2024-03-08 20:06:39 +0900

    [장유진] Footing CSS 추가 (24.03.08 20:06)

--- Nintenddo/client/src/Component/Footing.js
+++ Nintenddo/client/src/Component/Footing.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+// ㄴ 특정 컴포넌트로 이동하는 함수
 import '../Style/includes/footing.css'
 
 function Footing() {
@@ -43,23 +45,26 @@ function Footing() {
             </div>
             <div className='fcategory'>
                 <div className='intro'>
+                    <hr />
                     <div className='title'>한국닌텐도</div>
                     <div className='sub'>- 회사개요</div>
                     <div className='sub'>- 찾아오시는 길</div>
                 </div>
                 <div className='hardware'>
+                    <hr />
                     <div className='title'>본체/amiibo</div>
                     <div className='sub'>- Nintendo Switch</div>
                     <div className='sub'>- amiibo</div>
-
                 </div>
                 <div className='software'>
+                    <hr />
                     <div className='title'>소프트웨어</div>
                     <div className='sub'>- Nintendo Switch 소프트웨어</div>
                     <div className='sub'>- 소프트웨어 발매 스케줄</div>
                     <div className='sub'>- 스마트폰용 앱</div>
                 </div>
                 <div className='cutomer'>
+                    <hr />
                     <div className='title'>고객지원</div>
                     <div className='sub'>- Nintendo Switch</div>
                     <div className='sub'>- Nintendo 3DS</div>
@@ -68,6 +73,29 @@ function Footing() {
             </div>
 
             <div className='copyRight'>
+                <div className='copyContainer'>
+                    <div className='seller'>
+                        <Link style={{textDecoration: "none", color:"gray"}} to='/'>이용약관 &nbsp;</Link> |
+
+                        <Link style={{fontWeight:"bold", textDecoration: "none", color:"black"}}to='/'>&nbsp; 개인정보처리방침</Link>
+
+                        <br />
+
+                        ⓒ 2006 Nintendo of Korea Co., Ltd. All Rights Reserved.
+
+                        <br />
+
+                        상호명 : 한국닌텐도주식회사 &nbsp;&nbsp;대표자명 : 미우라 타카히로 &nbsp;&nbsp;사업자등록번호 : 120-87-03578
+                    </div>
+                    <div className='hours' style={{textAlign:"right"}}>
+                    고객지원 문의전화 : 1670-9900 (평일 오전 9시 30분~오후 5시 30분)
+
+                    <br /><br />
+
+                    ※ 토/일/공휴일/회사 정기휴일 및 특별휴일 제외
+
+                    </div>
+                </div>
 
             </div>
 
