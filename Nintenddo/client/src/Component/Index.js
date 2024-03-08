77624e34cad6b8958dc7fcf09750c2a6a8f1188e
JAVA <JAVA@DESKTOP-VHNHFON>
2024-03-08 14:43:15 +0900

    [장유진] 커밋

--- Nintenddo/client/src/Component/Index.js
+++ Nintenddo/client/src/Component/Index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Footing from './Footing'
 
 function Index() {
     return (
         <div>
             <h3>인덱스</h3>
-            <Footing />
         </div>
     )
 }
