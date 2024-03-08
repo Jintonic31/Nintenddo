3664cd84b63613c388e6e95d3174ce1759341ec2
dmlgn <dmlgn@DESKTOP-3I6BMRD>
2024-03-08 14:39:40 +0900

    훈 3.8 2:39분

--- Nintenddo/client/src/Component/Index.js
+++ Nintenddo/client/src/Component/Index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Heading from './Heading'
+import Style from '../Component/Style/index.css'
 
 function Index() {
     return (
         <div>
+            <Heading />
             <h3>인덱스</h3>
+            
         </div>
     )
 }
