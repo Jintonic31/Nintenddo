dbe3e2e51d46573dac5ccfd605c6953cbdfa4184
JAVA <JAVA@DESKTOP-VHNHFON>
2024-03-08 14:43:52 +0900

    ㄹㄹ

--- Nintenddo/client/src/Component/Index.js
+++ Nintenddo/client/src/Component/Index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footing from './Footing'
 
 function Index() {
     return (
         <div>
             <h3>인덱스</h3>
+            <Footing />
         </div>
     )
 }
