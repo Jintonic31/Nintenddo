20ed97ba6758e1f5ac8972799b6edb8b03bc791d
JAVA <JAVA@DESKTOP-VHNHFON>
2024-03-08 14:39:57 +0900

    [장유진] footing

---
+++ Nintenddo/client/src/Component/Footing.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../Style/'
+
+function Footing() {
+    return (
+        <div className='container'>
+            <div className='redline'>
+                <div className='flogo'>푸터<img src=''/></div>
+                <div className='goTop'></div>
+            </div>
+
+            <div className='snsLogin'>
+
+            </div>
+
+            <div className='fcategory'>
+
+            </div>
+        
+        </div>
+    )
+}
+
+export default Footing
