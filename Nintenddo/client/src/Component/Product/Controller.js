183f92e29ca759d837ec980cbf8628b3f3970ca3
JAVA <JAVA@DESKTOP-VHNHFON>
2024-03-14 19:42:31 +0900

    [장유진] cotroller 페이지 240314 1942

--- Nintenddo/client/src/Component/Product/Controller.js
+++ Nintenddo/client/src/Component/Product/Controller.js
@@ -7,6 +7,28 @@ import { Link } from 'react-router-dom'
 
 function Controller() {
 
+    const [redLine, setRedLine] = useState({display:"none"});
+    const [imgSrc, setImgSrc] = useState({
+        1: "http://localhost:8070/images/product/hardware/controllernav1.png",
+        3: "http://localhost:8070/images/product/hardware/controllernav3.png",
+        5: "http://localhost:8070/images/product/hardware/controllernav5.png"
+    })
+
+    const navItems = [
+        { id:1, text:"컨트롤러" },
+        { id:2, text:"Joy-Con" },
+        { id:3, text:"기타" },
+    ];
+
+    const handleMouseOver = (id) => {
+        setImgSrc({ ...imgSrc, [id]: `http://localhost:8070/images/product/hardware/controllernav${id + 1}.png` });
+        setRedLine({display:"flex"});
+    }
+
+    const handleMouseOut = (id) => {
+        setImgSrc({ ...imgSrc, [id]: `http://localhost:8070/images/product/hardware/controllernav${id}.png` });
+        setRedLine({display:"flex"});
+    }
 
     return (
         <div className='cnt'>
@@ -14,7 +36,22 @@ function Controller() {
             <Heading />
 
             <div className='controllerWrap'>
-                &nbsp;
+                
+                <div className='controllerNav'>
+                    <div className='nav'>
+                        <img src='http://localhost:8070/images/product/hardware/controllernav1.png' alt='' />
+                        컨트롤러
+                    </div>
+                    <div className='nav'>
+                        <img src='http://localhost:8070/images/product/hardware/controllernav2.png' alt='' />
+                        Joy-Con
+                    </div>
+                    <div className='nav'>
+                        <img src='http://localhost:8070/images/product/hardware/controllernav3.png' alt='' />
+                        충전기
+                    </div>
+                </div>
+
             </div>
         
             <Footing />
