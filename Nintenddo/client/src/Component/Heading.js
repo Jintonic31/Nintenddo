de2a797f2d42ce7b454380dd760ad36d6c125c73
dmlgn <dmlgn@DESKTOP-3I6BMRD>
2024-03-29 11:18:43 +0900

    봤나!! 정 USING!

--- Nintenddo/client/src/Component/Heading.js
+++ Nintenddo/client/src/Component/Heading.js
@@ -5,16 +5,32 @@ import Megadrop from './Megadrop';
 
 function Heading() {
   const navigate = useNavigate();
-  const [image, setImage] = useState({
-    1: 'image1',
-    3: 'image3',
-    5: 'image5',
-    7: 'image7',
-    9: 'image9',
-    11: 'image11',
-    13: 'image13'
+  const [imgSrc, setImgSrc] = useState({
+    1: "http://localhost:8070/images/includes/image1.png",
+    3: "http://localhost:8070/images/includes/image3.png",
+    5: "http://localhost:8070/images/includes/image5.png",
+    7: "http://localhost:8070/images/includes/image7.png",
+    9: "http://localhost:8070/images/includes/image9.png",
+    11: "http://localhost:8070/images/includes/image11.png",
+    13: "http://localhost:8070/images/includes/image13.png"
   });
+  const [dropStyle, setDropStyle] = useState({ display: "none" });
   const [activeCategoryId, setActiveCategoryId] = useState(null);
+
+  const handleMouseOver = (id) => {
+    setActiveCategoryId(id);
+    setImgSrc(prevImgSrc => ({
+      ...prevImgSrc,
+      [id]: `http://localhost:8070/images/includes/image${id + 1}.png`
+    }));
+    setDropStyle({ display: "flex" });
+  };
+
+  const handleMouseOut = (id) => {
+    setActiveCategoryId(null);
+    setImgSrc({ ...imgSrc, [id]: `http://localhost:8070/images/includes/image${id}.png` });
+    setDropStyle({ display: "none" });
+  };
 
   const menuItems = [
     { id: 1, text: "본체 및 amiibo", linkto:"/hardware" },
@@ -23,24 +39,8 @@ function Heading() {
     { id: 7, text: "캐릭터", linkto:"/character" },
     { id: 9, text: "고객지원", linkto:"/customer" },
     { id: 11, text: "장바구니", linkto:"/cartlist" },
-    { id: 13, text: "로그인", linkto:"/login" }
+    { id: 13, text: "검색", linkto:"/search" }
   ];
-  const [dropStyle, setDropStyle] = useState({ display: "none" });
-  const handleMouseOver = (id) => {
-    setImage({ ...image, [id]: `http://localhost:8070/images/includes/image${id + 1}.png` });
-  };
-
-  const dropMouseOver = (id) =>{
-    setDropStyle({ display: "flex" });
-  }
-
-  const handleMouseOut = (id) => {
-    setImage({ ...image, [id]: `http://localhost:8070/images/includes/image${id}.png` });
-  };
-
-  const dropMouseOut = (id) => {
-    setDropStyle({ display: "none" });
-  }
 
   return (
     <div className="category">
@@ -57,7 +57,7 @@ function Heading() {
         >
           <div className='incategory'>
             <img
-              src={`http://localhost:8070/images/includes/${image[id]}.png`}
+              src={imgSrc[id]}
               alt=''
             />
             {text}
@@ -67,6 +67,7 @@ function Heading() {
       <Megadrop
         navigate={navigate}
         categoryId={activeCategoryId}
+        dropStyle={dropStyle}
       />
     </div>
   );
